7eac15080ef8991aac14726da454b59ceb0b3d0d
Guajie <36125291+guajie@users.noreply.github.com>
2021-02-28 00:18:36 +0800

    Update KuWoMusicDownload.js

--- Scripts/KuWoMusicDownload.js
+++ Scripts/KuWoMusicDownload.js
@@ -11,7 +11,7 @@ QX1.0.0:
 #Vip script:
 ^https?:\/\/vip1\.kuwo\.cn\/(vip\/v2\/user\/vip|vip\/spi/mservice) url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/Kuwo.js
 
-Surge & QX MITM = vip1.kuwo.cn
+Surge & QX MITM = *.kuwo.cn
 */
 
 let url = $request.url.replace(/uid=\d+/g, "uid=2");
