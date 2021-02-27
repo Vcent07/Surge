163e6d588ca41682a65cce82b0192352fbfde0fc
Guajie <36125291+guajie@users.noreply.github.com>
2021-02-28 00:20:20 +0800

    Update Kuwo.js

--- Scripts/Kuwo.js
+++ Scripts/Kuwo.js
@@ -8,7 +8,7 @@ Surge4.0:
 http-response ^https?:\/\/vip1\.kuwo\.cn\/(vip\/v2\/user\/vip|vip\/spi/mservice) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/Kuwo.js
 http-request ^https?:\/\/musicpay\.kuwo.cn\/music\.pay\?uid=\d+ script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/KuWoMusicDownload.js
 
-MITM = vip1.kuwo.cn
+MITM = *.kuwo.cn
 */
 
 var body = $response.body;
@@ -20,13 +20,13 @@ const time = '/vip/spi/mservice';
 
 if (url.indexOf(vip) != -1) {
 	obj.data["isNewUser"] = "2";
-	obj.data["vipLuxuryExpire"] = "1835312949000";
-	obj.data["time"] = "1961170340993";
+	obj.data["vipLuxuryExpire"] = "4102500561000";
+	obj.data["time"] = "4102500561000";
 	obj.data["isYearUser"] = "2";
-	obj.data["vipmExpire"] = "1835312949000";
-	obj.data["vipOverSeasExpire"] = "1835312949000";
-	obj.data["vipExpire"] = "1835312949000";
-	obj.data["vip3Expire"] = "1835312949000";
+	obj.data["vipmExpire"] = "4102500561000";
+	obj.data["vipOverSeasExpire"] = "4102500561000";
+	obj.data["vipExpire"] = "4102500561000";
+	obj.data["vip3Expire"] = "4102500561000";
 	body = JSON.stringify(obj);
 }
 
