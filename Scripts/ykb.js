28997ccebad550fb9cda4233b69fd99516bdba47
Guajie <36125291+guajie@users.noreply.github.com>
2021-03-12 15:49:01 +0800

    Update ykb.js

--- Scripts/ykb.js
+++ Scripts/ykb.js
@@ -1,5 +1,18 @@
 /*
+
+Quantumult X
+
 Unlocks by Guajie
+
+[rewrite_remote]
+
+#医考帮
+https://api.yikaobang.com.cn/* url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/ykb.js
+
+[mitm]
+hostname = api.yikaobang.com.cn,
+
 */
+
 var body = $response.body.replace(/\u0069\u0073\u005F\u0076\u0069\u0070\u0022\u003A\u0022\u0030/g, '\u0069\u0073\u005F\u0076\u0069\u0070\u0022\u003A\u0022\u0031').replace(/\u0070\u0061\u0073\u0073\u0022\u003A\u0030/g, '\u0070\u0061\u0073\u0073\u0022\u003A\u0031')
 $done({ body });
