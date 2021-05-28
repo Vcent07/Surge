b63c9313dedc8f75e09d4fdaf67f06c6113eec56
Guajie <36125291+guajie@users.noreply.github.com>
2021-05-28 15:57:58 +0800

    Update jf_Convert.js

--- Scripts/jf_Convert.js
+++ Scripts/jf_Convert.js
@@ -1,7 +1,10 @@
 /*
 README：https://github.com/yichahucha/surge/tree/master
 ^https?://api\.m\.jd\.com/(client\.action|api)\?functionId=(wareBusiness|serverConfig|basicConfig|lite_wareBusiness|pingou_item)
- */
+
+# 如果京粉转链不生效，请检查重写是否订阅了神机去广告，然后把该订阅打开资源解析器，在订阅后面加#out=functionId即可；
+# 或者在本地重写[rewrite_local]添加一条 ^https?:\/\/api\.m\.jd.com\/client\.action\?functionId=start url reject-200
+*/
 
 const path1 = "serverConfig";
 const path2 = "wareBusiness";
